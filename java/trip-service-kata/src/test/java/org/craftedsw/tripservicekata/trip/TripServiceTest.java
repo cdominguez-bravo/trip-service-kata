f41f766bb673d2381685a1bf7cb107bdeb5e4d13
cdominguez_axity <cdominguez.bravo@gmail.com>
2020-10-04 12:42:29 -0300

    applying Test to Legacy Code Trip Service Kata

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,25 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
+import org.junit.Test;
+
+
 public class TripServiceTest {
-	
+
+  @Test(expected = UserNotLoggedInException.class)
+  public  void throws_an_exception_when_user_is_not_logged_in(){
+
+    TripService service = new TestableTripService();
+
+    service.getTripsByUser(null);
+  }
+
+  private class TestableTripService extends TripService{
+    @Override
+    protected User getLoggedInUser(){
+      return null;
+    }
+
+  }
 }
